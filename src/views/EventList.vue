37536875c7f38a0b99e263e5b40a19641e7849a2
maryitsv <msanchez@alertlogic.com>
2021-04-23 06:56:28 -0500

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="events">
+    <EventCard  v-for="event in events" :key=event.id :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+import axios from 'axios';
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null
+    }
+  },
+  created(){
+    axios.get(
+      'db.json'
+    ).then(
+      response => {
+        console.log("s", response);
+      }
+    ).catch(
+      error => {
+        console.log("s", error);
+      }
+    );
+
+  }
+};
+</script>
